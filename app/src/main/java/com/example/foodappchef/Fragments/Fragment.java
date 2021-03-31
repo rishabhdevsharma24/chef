14ad5896fa104f6915964f7842a7c0dfafaa5e85
rishabhdevsharma24 <rishabhdevsharma9@gmail.com>
2021-03-31 18:34:10 +0530

    updated chef and fragment

---
+++ app/src/main/java/com/example/foodappchef/Fragments/Fragment.java
@@ -0,0 +1,35 @@
+package com.example.foodappchef.Fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.foodappchef.R;
+
+public class Fragment  extends androidx.fragment.app.Fragment {
+    RecyclerView recyclerViewHorizontal, recyclerViewVertical;
+    Context context;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context = getContext();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        recyclerViewHorizontal = (RecyclerView) view.findViewById(R.id.home_Horizontal_recyclerView1);
+        recyclerViewVertical = (RecyclerView) view.findViewById(R.id.home_vertical_recyclerView1);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/foodappchef/Fragments/OnRecyclerViewItemClickListener.java
@@ -0,0 +1,7 @@
+package com.example.foodappchef.Fragments;
+
+import android.view.View;
+
+public interface OnRecyclerViewItemClickListener {
+    void onItemClick(int adapterPosition, View view);
+}
